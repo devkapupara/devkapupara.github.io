5c4981430fe8e52d101115bb8d9a8887be964816
Dev Kapupara <devkapupara@gmail.com>
2018-03-31 00:04:43 -0700

    logo

--- dependency/dynamic.js
+++ dependency/dynamic.js
@@ -1,23 +1,24 @@
 var title = $(".jumbotron h1")
 var hr = $('hr')
 
-var interval = setInterval(changeHrWidth, 8)
+var interval = setInterval(changeHrWidth, 12)
 
 title.animate({opacity:1}, 800, function() {
   title.animate({opacity:0}, 800, function() {
-    title.text("Meet Dev Kapupara")
-    title.animate({opacity:1}, 800)
+    title.text("Meet Dev Kapupara").animate({opacity:1}, 800)
   }).delay(400)
 }).delay(400)
 
 function changeHrWidth() {
   var width = parseInt(hr.css('width'))
-  if (width / parseInt(hr.parent().width()) >= 0.8)
+  var percent = width / parseInt(hr.parent().width())
+  if (percent >= 0.7)
+    $('.jumbotron h2').animate({opacity:1}, 800)
+  if (percent >= 0.8)
   {
     clearInterval(interval)
-    $(".jumbotron h2").animate({opacity:1}, 800)
     $('#content').animate({opacity: 1}, 800)
     $(".follow-me").animate({opacity:1}, 800)
   }
-  hr.css('width', width + 4 + 'px')
+  hr.css('width', width + 3 + 'px')
 }
