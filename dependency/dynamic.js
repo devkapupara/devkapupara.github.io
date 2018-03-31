25815deca43edbcdf6cec6930e0a3dc08bc4884d
Dev Kapupara <devkapupara@gmail.com>
2018-03-31 03:53:45 -0700

    transition fix

--- dependency/dynamic.js
+++ dependency/dynamic.js
@@ -5,15 +5,15 @@ var interval = setInterval(changeHrWidth, 15)
 
 title.animate({opacity:1}, 500, function() {
   title.animate({opacity:0}, 500, function() {
-    title.text("Meet Dev Kapupara").animate({opacity:1}, 500)
+    title.text("Meet Dev Kapupara").animate({opacity:1}, 800, function() {
+      $('.jumbotron h2').animate({opacity:1}, 500)
+    }).delay(400)
   }).delay(400)
 }).delay(400)
 
 function changeHrWidth() {
   var width = parseInt(hr.css('width'))
   var percent = width / parseInt(hr.parent().width())
-  if (percent >= 0.6)
-    $('.jumbotron h2').animate({opacity:1}, 500)
   if(percent >= 0.7)
   {
     clearInterval(interval)
