2d3689d32ea3a2ec2bc6040a05b8283ea59cae06
Dev Kapupara <devkapupara@gmail.com>
2018-03-31 23:11:07 -0700

    transition fix

--- dependency/dynamic.js
+++ dependency/dynamic.js
@@ -2,6 +2,7 @@ var title = $(".jumbotron h1")
 var hr = $('hr')
 var content = $('#content')
 var footer = $('.footer#content')
+
 var interval = setInterval(changeHrWidth, 15)
 
 title.animate({opacity:1}, 500, function() {
@@ -15,13 +16,13 @@ title.animate({opacity:1}, 500, function() {
 function changeHrWidth() {
   var width = parseInt(hr.css('width'))
   var percent = width / parseInt(hr.parent().width())
-  if(percent >= 0.6)
-  {
+  if(percent >= 0.7)
     content.animate({opacity: 1}, 500)
+  if (percent >=0.8)
+  {
+    clearInterval(interval)
     footer.animate({opacity: 1}, 500)
     $(".follow-me").animate({opacity:1}, 500)
   }
-  if (percent >=0.8)
-    clearInterval(interval)
   hr.css('width', width + 4 + 'px')
 }
